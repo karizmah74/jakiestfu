2fcc56fb60ee921e6513c60073bc45385a313a27
Jacob Kelley <4302888+karizmah74@users.noreply.github.com>
2015-04-30 09:08:22 -0700

    Tasks

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'less', '2.6.0'
---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function (grunt) {
+
+  var pkg = grunt.file.readJSON('package.json');
+  var banner = "/*! jakie shut the fuck up dot com */";
+
+  grunt.initConfig({
+
+    pkg: pkg,
+
+    clean: {
+      pre: {
+        src: ["dist"]
+      }
+    },
+
+    concat: {
+      tools: {
+        options: {
+          footer: "stfu.version = '<%= pkg.version %>';"
+        },
+        src: [
+          'src/js/**/*.js'
+        ],
+        dest: 'dist/js/stfu.js'
+      }
+    },
+
+    copy: {
+      html: {
+        files: [
+          {src: ['src/index.html'], dest: 'dist/index.html'},
+          {expand: true,  cwd: 'src/svg/', src: ['**/*'], dest: 'dist/svg/'}
+        ]
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'src/js/**/*.js']
+    },
+
+    less: {
+      prod: {
+        options: {
+          banner: banner + "\n",
+          compress: true,
+          sourcemap: 'none'
+        },
+        files: {
+          'dist/css/stfu.css': 'src/less/core.less'
+        }
+      }
+    },
+
+    uglify: {
+      prod: {
+        options: {
+          banner: banner + "\n",
+          preserveComments: 'none'
+        },
+        files: {
+          'dist/js/stfu.js': 'dist/js/stfu.js'
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        files: 'src/js/**/*.js',
+        tasks: ['jshint', 'concat', 'uglify']
+      },
+      less: {
+        files: 'src/less/**/*.less',
+        tasks: ['less']
+      },
+      html: {
+        files: 'src/**/*.html',
+        tasks: ['copy']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register Tasks
+  grunt.registerTask('default', ['clean', 'jshint', 'concat', 'uglify', 'copy', 'less']);
+  grunt.registerTask('develop', ['clean', 'jshint', 'concat', 'uglify', 'copy', 'less', 'watch']);
+};
