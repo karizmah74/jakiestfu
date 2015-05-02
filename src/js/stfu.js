9d350b9d3550ecaf5e36945791f797631a009b13
Jacob Kelley <4302888+karizmah74@users.noreply.github.com>
2015-05-01 21:07:40 -0700

    Tightening some code up

--- src/js/stfu.js
+++ src/js/stfu.js
@@ -1,4 +1,18 @@
-(function() {
-  var Namespace = 'stfu';
-  this[Namespace] = function() {};
-}).call(this);
+window.onload = function() {
+
+  // Branding
+  SVGInjector(document.getElementById('brand'));
+
+  // Themes
+  //var themes = ["slate", "peach", "teal", "shore"];
+  //document.body.className += " " + themes[Math.floor(Math.random()*themes.length)];
+
+  // Classes
+  var transitions = document.querySelectorAll('.transition');
+  for(var i = 0; i<transitions.length; i++) {
+    transitions[i].className += " t" + (i+1);
+  }
+
+  // Show
+  document.body.className += " view";
+};
